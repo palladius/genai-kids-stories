956fbd5890f4455f744ce06b75586dbd5c8d435b
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-06-25 22:06:38 +0200

    Better ENV files

--- Makefile
+++ Makefile
@@ -58,18 +58,11 @@ docker-build:
 
 
 docker-run-bash-nobuild:
-	docker run -it -p 30080:3000 \
-		-e PROJECT_ID=$(PROJECT_ID) \
-		-e APPLICATION_DEFAULT_CREDENTIALS=/sa.json \
-		-e APP_DB_NAME=$(APP_DB_NAME) \
-		-e APP_DB_USER=$(APP_DB_USER) \
-		-e APP_DB_PASS=$(APP_DB_PASS) \
-		-e APP_DB_HOST=$(APP_DB_HOST) \
-		 "$(APP_NAME)":v`bin/version.sh` bash
-	# bundle exec rails s
+	bin/docker-run-locally.sh bash
+
 # No such file or directory - gcloud
 docker-run-nobuild:
-	docker run -it -p 30080:3000 -e PROJECT_ID=$(PROJECT_ID) "$(APP_NAME)":v`bin/version.sh`  bundle exec rails s
+	bin/docker-run-locally.sh bundle exec rails s -b 0.0.0.0
 
 docker-run: docker-build docker-run-nobuild
 docker-run-bash: docker-build docker-run-bash-nobuild
--- bin/docker-run-locally.sh
+++ bin/docker-run-locally.sh
@@ -3,9 +3,26 @@
 set -euo pipefail
 # find this
 
+set -euo pipefail
+
 set -x
 
-docker run -p 3000:3000 \
-    -e ENV APPLICATION_DEFAULT_CREDENTIALS=/sa.json \
-    -e RAILS_ENV=dev-on-gcp \
-    rails s -b 0.0.0.0
+export DEFAULT_ENV=''
+
+# docker run -it -p 3000:3000 \
+#     rails s -b 0.0.0.0
+
+
+docker run -it -p 30080:3000 \
+		-e PROJECT_ID="$PROJECT_ID" \
+        -e RAILS_ENV=dev-on-gcp \
+		-e RAILS_MASTER_KEY="$(cat config/master.key)"  \
+		-e APPLICATION_DEFAULT_CREDENTIALS=/sa.json \
+		-e APP_DB_NAME="$APP_DB_NAME" \
+		-e APP_DB_USER="$APP_DB_USER" \
+		-e APP_DB_PASS="$APP_DB_PASS" \
+		-e APP_DB_HOST="$APP_DB_HOST" \
+		 "$APP_NAME":v`bin/version.sh` \
+		${*:-rails s -b 0.0.0.0}
+
+#	docker run -it -p 30080:3000 -e PROJECT_ID=$PROJECT_ID "$(APP_NAME)":v`bin/version.sh`
