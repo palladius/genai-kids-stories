4d247e5b4ea4a33edf12b22ad9e221ec894a65b1
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-07-03 08:43:14 +0200

    fixed client

--- test/controllers/translated_stories_controller_test.rb
+++ test/controllers/translated_stories_controller_test.rb
@@ -1,45 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class TranslatedStoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @translated_story = translated_stories(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get translated_stories_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_translated_story_url
     assert_response :success
   end
 
-  test "should create translated_story" do
-    assert_difference("TranslatedStory.count") do
-      post translated_stories_url, params: { translated_story: { client_id: @translated_story.client_id, genai_model: @translated_story.genai_model, internal_notes: @translated_story.internal_notes, language: @translated_story.language, name: @translated_story.name, paragraph_strategy: @translated_story.paragraph_strategy, story_id: @translated_story.story_id, user_id: @translated_story.user_id } }
+  test 'should create translated_story' do
+    assert_difference('TranslatedStory.count') do
+      post translated_stories_url,
+           params: { translated_story: { kid_id: @translated_story.kid_id, genai_model: @translated_story.genai_model,
+                                         internal_notes: @translated_story.internal_notes, language: @translated_story.language, name: @translated_story.name, paragraph_strategy: @translated_story.paragraph_strategy, story_id: @translated_story.story_id, user_id: @translated_story.user_id } }
     end
 
     assert_redirected_to translated_story_url(TranslatedStory.last)
   end
 
-  test "should show translated_story" do
+  test 'should show translated_story' do
     get translated_story_url(@translated_story)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_translated_story_url(@translated_story)
     assert_response :success
   end
 
-  test "should update translated_story" do
-    patch translated_story_url(@translated_story), params: { translated_story: { client_id: @translated_story.client_id, genai_model: @translated_story.genai_model, internal_notes: @translated_story.internal_notes, language: @translated_story.language, name: @translated_story.name, paragraph_strategy: @translated_story.paragraph_strategy, story_id: @translated_story.story_id, user_id: @translated_story.user_id } }
+  test 'should update translated_story' do
+    patch translated_story_url(@translated_story),
+          params: { translated_story: { kid_id: @translated_story.kid_id, genai_model: @translated_story.genai_model,
+                                        internal_notes: @translated_story.internal_notes, language: @translated_story.language, name: @translated_story.name, paragraph_strategy: @translated_story.paragraph_strategy, story_id: @translated_story.story_id, user_id: @translated_story.user_id } }
     assert_redirected_to translated_story_url(@translated_story)
   end
 
-  test "should destroy translated_story" do
-    assert_difference("TranslatedStory.count", -1) do
+  test 'should destroy translated_story' do
+    assert_difference('TranslatedStory.count', -1) do
       delete translated_story_url(@translated_story)
     end
 
--- test/system/translated_stories_test.rb
+++ test/system/translated_stories_test.rb
@@ -1,55 +1,55 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TranslatedStoriesTest < ApplicationSystemTestCase
   setup do
     @translated_story = translated_stories(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit translated_stories_url
-    assert_selector "h1", text: "Translated stories"
+    assert_selector 'h1', text: 'Translated stories'
   end
 
-  test "should create translated story" do
+  test 'should create translated story' do
     visit translated_stories_url
-    click_on "New translated story"
+    click_on 'New translated story'
 
-    fill_in "Client", with: @translated_story.client_id
-    fill_in "Genai model", with: @translated_story.genai_model
-    fill_in "Internal notes", with: @translated_story.internal_notes
-    fill_in "Language", with: @translated_story.language
-    fill_in "Name", with: @translated_story.name
-    fill_in "Paragraph strategy", with: @translated_story.paragraph_strategy
-    fill_in "Story", with: @translated_story.story_id
-    fill_in "User", with: @translated_story.user_id
-    click_on "Create Translated story"
+    fill_in 'Kid', with: @translated_story.kid_id
+    fill_in 'Genai model', with: @translated_story.genai_model
+    fill_in 'Internal notes', with: @translated_story.internal_notes
+    fill_in 'Language', with: @translated_story.language
+    fill_in 'Name', with: @translated_story.name
+    fill_in 'Paragraph strategy', with: @translated_story.paragraph_strategy
+    fill_in 'Story', with: @translated_story.story_id
+    fill_in 'User', with: @translated_story.user_id
+    click_on 'Create Translated story'
 
-    assert_text "Translated story was successfully created"
-    click_on "Back"
+    assert_text 'Translated story was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Translated story" do
+  test 'should update Translated story' do
     visit translated_story_url(@translated_story)
-    click_on "Edit this translated story", match: :first
+    click_on 'Edit this translated story', match: :first
 
-    fill_in "Client", with: @translated_story.client_id
-    fill_in "Genai model", with: @translated_story.genai_model
-    fill_in "Internal notes", with: @translated_story.internal_notes
-    fill_in "Language", with: @translated_story.language
-    fill_in "Name", with: @translated_story.name
-    fill_in "Paragraph strategy", with: @translated_story.paragraph_strategy
-    fill_in "Story", with: @translated_story.story_id
-    fill_in "User", with: @translated_story.user_id
-    click_on "Update Translated story"
+    fill_in 'Kid', with: @translated_story.kid_id
+    fill_in 'Genai model', with: @translated_story.genai_model
+    fill_in 'Internal notes', with: @translated_story.internal_notes
+    fill_in 'Language', with: @translated_story.language
+    fill_in 'Name', with: @translated_story.name
+    fill_in 'Paragraph strategy', with: @translated_story.paragraph_strategy
+    fill_in 'Story', with: @translated_story.story_id
+    fill_in 'User', with: @translated_story.user_id
+    click_on 'Update Translated story'
 
-    assert_text "Translated story was successfully updated"
-    click_on "Back"
+    assert_text 'Translated story was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Translated story" do
+  test 'should destroy Translated story' do
     visit translated_story_url(@translated_story)
-    click_on "Destroy this translated story", match: :first
+    click_on 'Destroy this translated story', match: :first
 
-    assert_text "Translated story was successfully destroyed"
+    assert_text 'Translated story was successfully destroyed'
   end
 end
