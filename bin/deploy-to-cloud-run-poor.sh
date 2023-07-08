c59f9af11beac6318a789f0b0de1914581a0a1c0
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-07-08 15:15:58 +0200

    0.11.15 15 put correct project_id

--- bin/deploy-to-cloud-run-poor.sh
+++ bin/deploy-to-cloud-run-poor.sh
@@ -7,6 +7,8 @@ set -euo pipefail
 echo 'Hi Im a brand new script which needs to be called from Cloud Build and needs to be told ALL variables excplitily'
 echo 'Ill start with all being manual and then move to variables one piece at a time'
 
+# this is NOT the build project..
+export PROJECT_ID=ricc-genai
 export ARTIFACT_REPO='europe-north1-docker.pkg.dev/ror-goldie/genai'
 UPLOADED_IMAGE="$ARTIFACT_REPO/genai-kids-stories"
 UPLOADED_IMAGE_WITH_VER="$UPLOADED_IMAGE:cb-latest"
