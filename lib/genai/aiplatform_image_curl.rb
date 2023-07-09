4321617c66f5451d21d03beeb7b787ce5d75717d
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-07-09 18:55:21 +0200

    0.11.20b minor changes to CB

--- lib/genai/aiplatform_image_curl.rb
+++ lib/genai/aiplatform_image_curl.rb
@@ -200,7 +200,7 @@ module Genai
       # puts 'prediction_size_minus_one: ', prediction_size_minus_one
       (0..prediction_size_minus_one).each do |ix|
         # ret_hash["#{ix}_start"] = 'debug'
-        filename = "tmp_#{_model_version}-#{content.gsub(/ /, '_').gsub(/\*/, '')[0, 100]}.ix=#{ix}.png"
+        filename = "tmp_#{_model_version}-#{content.gsub(/ /, '_').gsub(/[^a-zA-Z0-9_]/, '')[0, 100]}.ix=#{ix}.png"
         # cant be too long or you get File name too long @ rb_sysopen - tmp_002-Imagine_a_5-year-old_brown-eyed_boy_with_light_brown_hair._Beside_him,_**Act_1**
         # filename = "tmp_#{_model_version}-#{content.gsub(/ /, '_')[0, 100]}.ix=#{ix}.png"
 
